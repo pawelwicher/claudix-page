9037cd2bd55293a5e5ee285c6db02ec687f3aa53
Paweł Wicher <pwi@epcon.pl>
2020-09-25 15:43:04 +0200

    simple action added

--- src/App.fs
+++ src/App.fs
@@ -1,6 +1,7 @@
 module App
 
 open Fable.React
+open Fable.React.Props
 open Elmish
 open Elmish.React
 
@@ -12,21 +13,28 @@ type Model =
     Address: string
   }
 
-let initialModel: Model = {
-  Name = "Claudix Paweł Wicher"
-  Nip = "NIP 6121799181"
-  Regon = "REGON 363264887"
-  Address = "ul. Pochyła, nr 21, lok. 7A, 53-512 Wrocław"
-}
+let initialModel: Model = 
+  {
+    Name = "Claudix Paweł Wicher"
+    Nip = ""
+    Regon = ""
+    Address = ""
+  }
 
 type Msg =
-  | Noop
+  | ShowDetails
+  | HideDetails
 
 let init () =
-  initialModel, Cmd.none
+  initialModel,
+  Cmd.none
 
 let update (msg: Msg) (model: Model) =
-  model, Cmd.none
+  match msg with
+  | ShowDetails ->
+    {  model with Nip = "NIP 6121799181"; Regon = "REGON 363264887"; Address = "ul. Pochyła, nr 21, lok. 7A, 53-512 Wrocław" }, Cmd.none
+  | HideDetails ->
+    init ()
 
 let view (model: Model) dispatch =
   div []
@@ -38,6 +46,10 @@ let view (model: Model) dispatch =
       span [] [ str model.Regon ]
       br []
       span [] [ str model.Address ]
+      br []
+      button  [ OnClick (fun e -> dispatch ShowDetails) ] [ str "show" ]
+      br []
+      button  [ OnClick (fun e -> dispatch HideDetails) ] [ str "hide" ]
     ]
 
 Program.mkProgram init update view
